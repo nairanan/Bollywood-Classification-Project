83696d39cbb066cab465c845b06b3b72e86e1a06
nairanan <nairanan@umich.edu>
2025-07-16 16:21:17 -0400

    flask server classify function

--- Server/server.py
+++ Server/server.py
@@ -1,11 +1,19 @@
 from flask import Flask, request, jsonify
-#import util
+import utility
 
 app = Flask(__name__)
 
 @app.route('/classify_image', methods = ['GET', 'POST'])
 def classify_image():
-    return "hello"
+    image_data = request.form('image_data')
+
+    response = jsonify(utility.classify_image(image_data))
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
 
 if __name__ == "__main__":
+    print("Starting Server for Bollywood Actor Classification")
+    utility.load_saved_artifacts()
     app.run(port=5000)
